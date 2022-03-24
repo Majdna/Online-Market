c0d7aaa1e2eca150176ef82338a1de8aa5f146bf
Majdna <48679257+Majdna@users.noreply.github.com>
2022-03-24 12:08:21 +0200

    fix route problem

--- server/server.ts
+++ server/server.ts
@@ -24,28 +24,11 @@ app.get("/", (req,res)=>{
  {useNewUrlParser: true},
  ()=>console.log('connected to DB !!')
  );
-// mongoose.connect('mongodb+srv://Alaa1:yEszVzboZEnwamwT@cluster0.es9yf.mongodb.net/test');
-// const db = mongoose.connection;
-// const userSchema = new mongoose.Schema({
-//   name: String,
-//   password: String,
-// });
 
-// async function main() {
-//   const password = process.env.MONGODB_PASSWORD;
-//   await mongoose.connect(`mongodb+srv://Alaa1:yEszVzboZEnwamwT@cluster0.es9yf.mongodb.net/priviteProjectDatabase?retryWrites=true&w=majority`);
-// }
+app.get('/*',(req, res)=>{
+  res.sendFile(__dirname + '/../client/build/index.html');
+})
 
-// const User = mongoose.model('appointment', userSchema);
-// const event = new User({ name: 'asma', password:"123" });
-// console.log(event.name);
-// event.save();
-
-// db.on("error", console.error.bind(console, "connection error:"));
-// db.once("open", () => {
-//   console.log("connected to DB!");
-// });
-// app.use("/product",products);
  app.listen(port, () => {
    return console.log(`Express is listening at http://localhost:${port}`);
 });

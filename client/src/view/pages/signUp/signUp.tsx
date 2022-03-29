85fa7fbf38a13db224c3796544dfed7f4fe23f09
Majdna <48679257+Majdna@users.noreply.github.com>
2022-03-29 19:39:50 +0300

    update navbar + signup

--- client/src/view/pages/signUp/signUp.tsx
+++ client/src/view/pages/signUp/signUp.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Grid, Paper, Avatar, Typography, TextField, Button } from '@material-ui/core'
 import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
 import Radio from '@material-ui/core/Radio';
@@ -7,19 +7,44 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import group from '../Groups/group';
 import axios from 'axios';
 const signUp = () => {
     // add use State for user 
+    const nav = useNavigate();
+    const [email, setEmail] = useState("");
+    const [Name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
+    const [Id, setId] = useState("");
 
+    
     const paperStyle = { padding: '30px 20px', width: 300, margin: "20px auto" }
     const headerStyle = { margin: 0 }
     const avatarStyle = { backgroundColor: '#1bbd7e' }
     const marginTop = { marginTop: 5 }
-    function handelLink(e:any){
-        e.preventDefault();
-        
+    
+    interface user {
+        id: string;
+        FullName: string;
+        password: string;
+        phone: string;
+        Email:string;
+    }
+
+
+    async function handelLink(e:any){
+        const _user:user = {"id":Id ,"FullName":Name,"password":password,"phone":phone,"Email":email}
+        axios.post(`http://localhost:4001/users`, _user)
+        .then(res => {
+          console.log(res);
+          console.log(res.data);
+        })
+
+        alert("Signed Up :)")
+        nav("/store");
+       
     }
     return (
         <Grid>
@@ -31,8 +56,9 @@ const signUp = () => {
                     <h2 style={headerStyle}>Sign Up</h2>
                     </Grid>
                 <form onSubmit={handelLink} >
-                    <TextField fullWidth label='Name' required placeholder="Enter your name" />
-                    <TextField fullWidth label='Email' required placeholder="Enter your email" />
+                    <TextField fullWidth label='Id' required placeholder="Enter your ID" onKeyUp={(e: any) => setId(e.target.value)}/>
+                    <TextField fullWidth label='Name' required placeholder="Enter your name" onKeyUp={(e: any) => setName(e.target.value)}/>
+                    <TextField fullWidth label='Email' required placeholder="Enter your email" onKeyUp={(e: any) => setEmail(e.target.value)}/>
                     <FormControl component="fieldset" required style={marginTop}>
                         <FormLabel component="legend">Gender</FormLabel>
                         <RadioGroup aria-label="gender"  name="gender" style={{ display: 'initial' }}>
@@ -40,8 +66,8 @@ const signUp = () => {
                             <FormControlLabel value="male" control={<Radio />} label="Male" />
                         </RadioGroup>
                     </FormControl>
-                    <TextField fullWidth label='Phone Number' required placeholder="Enter your phone number" />
-                    <TextField fullWidth type="password" label='Password' required placeholder="Enter your password"/>
+                    <TextField fullWidth label='Phone Number' required placeholder="Enter your phone number" onKeyUp={(e: any) => setPhone(e.target.value)}/>
+                    <TextField fullWidth type="password" label='Password' required placeholder="Enter your password" onKeyUp={(e: any) => setPassword(e.target.value)}/>
                     <TextField fullWidth  type="password"  label='Confirm Password' required placeholder="Confirm your password"/>
                    
                     <Button type='submit' onClick={handelLink}  variant='contained' color='primary'> Sign up</Button>
